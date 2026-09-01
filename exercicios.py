d8cd48efb977b71425fb6df0c43c0d29782a73d3
matheus <matheusosf11@gmail.com>
2026-08-31 21:30:06 -0300

    Initial commit

---
+++ exercicios.py
@@ -0,0 +1,5 @@
+anoNascimento=int(input("Digite o ano do seu nascimento: "))
+anoAtual=int(input("Digite o ano em que estamos:"))
+
+idade = anoAtual-anoNascimento
+print("Sua idade é ",idade,"anos")
---
+++ teste.py
@@ -0,0 +1,8 @@
+nome=input("Digite seu nome: ")
+nota1=float(input("Digite a primeira nota: "))
+nota2=float(input("Digite a segunda nota: "))
+nota3=float(input("Digite a terceira nota: "))
+
+media=(nota1+nota2+nota3)/3
+
+print("Olá,",nome,",a sua média final doi de ",media)
