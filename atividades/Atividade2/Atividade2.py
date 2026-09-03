5ae88e7f272496d02c90ac00cdd33e9b186c1af6
matheus <matheusosf11@gmail.com>
2026-09-02 21:40:58 -0300

    Atividade 3 02/09

--- atividades/Atividade2/Atividade2.py
+++ atividades/Atividade2/Atividade2.py
@@ -1,7 +1,7 @@
 nome=input("Digite seu nome: ")
 idade=int(input("Digite sua idade: "))
-plano=input("Tem plano de saúde? 'Sim' ou 'Não'")
+plano=input("Tem plano de saúde?\n1)Sim  2)Não  ")
+plano_de_saude= plano=="1"
+print("Seu nome é",nome,",Você tem",idade,"anos.\nTem plano?",plano_de_saude,"\nVocê foi aceito?",plano_de_saude)
 
-if plano == 'Sim':
-    print("")
 
